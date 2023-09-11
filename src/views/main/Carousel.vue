16b2f20d4fdbf0056bcb5b391f1164e1a9bf8f19
Gu_ZT <administration@ztxy666.cn>
2023-09-11 13:13:34 +0800

    Update Carousel.vue

--- src/views/main/Carousel.vue
+++ src/views/main/Carousel.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 import {ref} from "vue";
+
 const pictures = ref(['p0000', 'p0001', 'p0002', 'p0003', 'p0004', 'p0005', 'p0006', 'p0007']);
 </script>
 
 <template>
   <el-carousel class="carousel" :interval="4000" type="card" height="400px" arrow="never" indicator-position="none">
     <el-carousel-item v-for="picture in pictures" :key="picture">
-      <el-image :src="`https://raw.githubusercontent.com/XATU-MC/.github/main/src/assets/carousel/${picture}.png`"/>
+      <el-image :src="`https://raw.githubusercontent.com/XATU-MC/.github/main/src/assets/carousel/${picture}.png`"
+                style="height: 400px;"/>
     </el-carousel-item>
   </el-carousel>
 </template>
 
 <style scoped lang="scss">
-.carousel{
+.carousel {
   margin: 15px;
 }
 </style>
