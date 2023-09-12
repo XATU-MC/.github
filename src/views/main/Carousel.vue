d351246f800ac671a724e6261cba497871d9e215
Gu_ZT <administration@ztxy666.cn>
2023-09-13 05:54:00 +0800

    Update Carousel

--- src/views/main/Carousel.vue
+++ src/views/main/Carousel.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-const pictures = ref(['p0000', 'p0001.png', 'p0002', 'p0003', 'p0004', 'p0005', 'p0006', 'p0007', 'p0008', 'p0009', 'p0010']);
+const pictures = ref([
+  'https://attachment.mcbbs.net/data/myattachment/forum/202309/13/054553vhzfmzeya34mc8fq.png',
+  'https://attachment.mcbbs.net/data/myattachment/forum/202309/13/054555wjspd7du6efpfm56.png',
+  'https://attachment.mcbbs.net/data/myattachment/forum/202309/13/054558q6f3fntnnxltzdti.png',
+  'https://attachment.mcbbs.net/data/myattachment/forum/202309/13/054601jiktnlk4akoqkqol.png',
+  'https://attachment.mcbbs.net/data/myattachment/forum/202309/13/054604ws8f3430833ezk7u.png',
+  'https://attachment.mcbbs.net/data/myattachment/forum/202309/13/054606pt6hreb4au14saft.png',
+  'https://attachment.mcbbs.net/data/myattachment/forum/202309/13/054608karkeve1i7z0v271.png',
+  'https://attachment.mcbbs.net/data/myattachment/forum/202309/13/054611d8wiqibqpq3snbng.png',
+  'https://attachment.mcbbs.net/data/myattachment/forum/202309/13/054613onmptpkx5nnlxunp.png',
+  'https://attachment.mcbbs.net/data/myattachment/forum/202309/13/054616zesl337bcyicspt8.png',
+  'https://attachment.mcbbs.net/data/myattachment/forum/202309/13/054618esf00bttzzqatcto.png'
+]);
 </script>
 
 <template>
-  <el-carousel class="carousel" :interval="4000" type="card" height="400px" arrow="never" indicator-position="none">
+  <el-carousel class="carousel" :interval="4000" height="400px" type="card" arrow="never" indicator-position="none">
     <el-carousel-item v-for="picture in pictures" :key="picture">
-      <el-image :src="`https://gitee.com/xatumc/.gitee/raw/main/src/assets/carousel/${picture}.png`"
-                style="height: 400px;"/>
+      <el-image :src="picture"/>
     </el-carousel-item>
   </el-carousel>
 </template>
